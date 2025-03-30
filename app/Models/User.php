567b16514904d9f4eeda06cb8ea069a203ae6581
OUSSAMAEDDERKAOUI <edderkaouioussama@gmail.com>
2025-03-30 23:27:42 +0000

    La creation de migration de table Ligues

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,16 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'prenom',
+        'nom',
+        'nationalite',
+        'date_naissance',
+        'telephone',
+        'photo',
+        'status',
         'email',
         'password',
+        'role',
     ];
 
     /**
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,6 @@ return new class extends Migration
             $table->string('telephone');
             $table->string('photo');
             $table->enum('status',['actif','inactif']);
-
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
