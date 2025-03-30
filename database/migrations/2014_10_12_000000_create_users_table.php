a1b00ed94a652396e196cbe3c63f679eee15932d
OUSSAMAEDDERKAOUI <edderkaouioussama@gmail.com>
2025-03-30 23:17:45 +0000

    La creation de migration de table users.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,18 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('prenom');
+            $table->string('nom');
+            $table->string('nationalite');
+            $table->date('date_naissance');
+            $table->string('telephone');
+            $table->string('photo');
+            $table->enum('status',['actif','inactif']);
+
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['joueur', 'arbitre', 'entraineur', 'medecin']);
             $table->rememberToken();
             $table->timestamps();
         });
