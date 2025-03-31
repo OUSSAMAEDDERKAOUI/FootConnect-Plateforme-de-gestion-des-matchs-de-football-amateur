5657deffcfd7a0b12610925f327138a1371863cf
OUSSAMAEDDERKAOUI <edderkaouioussama@gmail.com>
2025-03-31 00:15:04 +0000

    La creation de migration de table medecins avec MedecinFactory .

---
+++ database/factories/MedecinFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Medecin;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class MedecinFactory extends Factory
+{
+    // Le modèle associé à la factory
+    protected $model = Medecin::class;
+
+    /**
+     * Définir l'état de la factory.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'licence' => $this->faker->unique()->bothify('???-####-???'), 
+            'specialite' => $this->faker->randomElement(['Traumatologie', 'Physiothérapie', 'Médecine générale']), 
+            'statut' => $this->faker->randomElement(['Actif', 'Inactif']), 
+        ];
+    }
+}
