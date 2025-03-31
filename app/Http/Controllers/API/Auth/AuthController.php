3eff996ef0976aa691708a58d9e3ba04839c2246
OUSSAMAEDDERKAOUI <edderkaouioussama@gmail.com>
2025-03-31 23:56:15 +0000

    La creation de Login request

--- app/Http/Controllers/API/Auth/AuthController.php
+++ app/Http/Controllers/API/Auth/AuthController.php
@@ -1,11 +1,97 @@
 <?php
-
 namespace App\Http\Controllers\API\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\RegisterUserRequest;
+use App\Http\Requests\Auth\LoginUserRequest;
+use App\Services\UserService;
+use Illuminate\Support\Facades\Auth;
+use Spatie\LaravelIgnition\Recorders\DumpRecorder\Dump;
 
 class AuthController extends Controller
 {
-    //
+    protected $userService;
+
+    public function __construct(UserService $userService)
+    {
+        $this->userService = $userService;
+    }
+
+    public function register(RegisterUserRequest $request)
+    {
+        $validatedData = $request->validated();
+        // Dump($validatedData);
+        $user = $this->userService->registerUser($validatedData);
+
+        if ($user) {
+            $token = Auth::guard('api')->login($user);
+
+            return response()->json([
+                'status' => 'success',
+                'message' => 'User created successfully',
+                'user' => $user,
+                'authorisation' => [
+                    'token' => $token,
+                    'type' => 'bearer',
+                ]
+            ]);
+        }
+
+        return response()->json([
+            'error' => 'The email address is already registered. Please choose a different email.',
+        ], 422);
+    }
+
+
+    public function login(LoginUserRequest $request)
+    {
+        $request->validated();
+
+        $credentials = $request->only('email', 'password');
+
+        $result = $this->userService->authenticate($credentials);
+
+        if (!$result) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Unauthorized',
+            ], 401);
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'user' => $result['user'],
+            'authorisation' => [
+                'token' => $result['token'],
+                'type' => 'bearer',
+            ]
+        ]);
+    }
+
+    public function logout()
+    {
+        $this->userService->logout();
+
+        // Retourner une réponse de succès après la déconnexion
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Successfully logged out',
+        ]);
+    }
+
+
+//     // public function refresh()
+//     // {
+//     //     return response()->json([
+//     //         'status' => 'success',
+//     //         'user' => Auth::guard('api')->user(),
+//     //         'authorisation' => [
+//     //             'token' => Auth::guard('api')->refresh(),
+//     //             'type' => 'bearer',
+//     //         ]
+//     //     ]);
+//     // }
+
+
+    
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Models;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject; 
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -8,7 +9,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject 
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -57,4 +58,23 @@ public function arbitre(){
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+
+
+
+
+  
+   public function getJWTIdentifier()
+    {
+      return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+      return [
+        'email'=>$this->email,
+        'name'=>$this->name
+      ];
+    }
 }
+
