37ac67854813d7ea24c3156e32536751ba0a6e41
OUSSAMAEDDERKAOUI <edderkaouioussama@gmail.com>
2025-04-04 11:03:35 +0000

    add  sanction model with all rolations

---
+++ app/Models/Sanction.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sanction extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'game_id', 
+        'joueur_id', 
+        'type', 
+        'date_time', 
+        'duree', 
+        'note',
+    ];
+
+    public function game()
+    {
+        return $this->belongsTo(Game::class);
+    }
+
+    public function joueur()
+    {
+        return $this->belongsTo(Joueur::class);
+    }
+}
