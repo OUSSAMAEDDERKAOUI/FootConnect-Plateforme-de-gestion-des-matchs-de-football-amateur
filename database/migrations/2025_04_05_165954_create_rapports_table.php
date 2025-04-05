62f31faaf08716a5d96cf22cb7f7150e7632b661
OUSSAMAEDDERKAOUI <edderkaouioussama@gmail.com>
2025-04-05 17:05:47 +0000

    La creation de la migration de la  table Rapports

---
+++ database/migrations/2025_04_05_165954_create_rapports_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rapports', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('game_id');
+            $table->text('reserves')->nullable();
+            $table->text('observations')->nullable();
+            $table->timestamps();
+
+            $table->foreign('game_id')->references('id')->on('games')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rapports');
+    }
+};
