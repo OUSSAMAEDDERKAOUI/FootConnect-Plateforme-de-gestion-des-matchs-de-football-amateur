4e85bc073dbb8595b73407eb608a3b10b8a68a39
OUSSAMAEDDERKAOUI <edderkaouioussama@gmail.com>
2025-04-05 18:36:53 +0000

    La creation de BlessureRepository

---
+++ app/Repositories/BlessureRepository/BlessureRepository.php
@@ -0,0 +1,43 @@
+<?php 
+
+namespace App\Repositories\BlessureRepository;
+
+use App\Models\Blessure;
+use App\Repositories\BlessureRepository\BlessureRepositoryInterface;
+
+class BlessureRepository implements BlessureRepositoryInterface
+{
+    protected $model;
+
+    public function __construct(Blessure $blessure)
+    {
+        $this->model = $blessure;
+    }
+
+    public function all()
+    {
+        return $this->model->all();
+    }
+
+    public function find($id)
+    {
+        return $this->model->find($id);
+    }
+
+    public function create(array $data)
+    {
+        return $this->model->create($data);
+    }
+
+    public function update($id, array $data)
+    {
+        $blessure = $this->model->find($id);
+        return $blessure->update($data);
+    }
+
+    public function delete($id)
+    {
+        $blessure = $this->model->find($id);
+        return $blessure->delete();
+    }
+}
